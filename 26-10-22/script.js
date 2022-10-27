603a5f6ff5c3068cfdf3014cf028f846e48a7514
pietrodistefano23 <pietrodistefano2306@gmail.com>
2022-10-27 09:37:55 +0200

    add new project 26-10-22

---
+++ 26-10-22/script.js
@@ -0,0 +1,41 @@
+
+//######Dichiarazione variabili#####
+
+let datiArr = ["Pietro","Di Stefano",20,"diploma"];
+console.log(datiArr);
+
+//######Esercizio 1#############
+
+datiArr.splice (2,1,"23/06/02");
+console.log(datiArr);
+
+//######Esercizio 2#############
+
+let up = "Pietro, Di Stefano ";
+up = up.toUpperCase();
+datiArr.splice(0,2, up);
+console.log(datiArr);
+
+let num = 7;
+let src = "";
+
+for(let i=0;i<num;i++){
+    for(let j=0;j<num-i;j++){
+        src += "#";
+    }
+    src += "\n";
+}
+console.log(src);
+
+
+
+
+
+
+
+
+
+
+
+
+  
