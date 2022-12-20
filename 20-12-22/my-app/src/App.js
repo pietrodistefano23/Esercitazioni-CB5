26d3262f7072481c9385689111a2480a00c3291d
pietrodistefano23 <pietrodistefano2306@gmail.com>
2022-12-20 16:30:45 +0100

    add new project 20-12-22

---
+++ 20-12-22/my-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Button from './Button';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello World</p>
+        <Button />
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ 20-12-22/my-app/src/Button.js
@@ -0,0 +1,12 @@
+
+
+function Button(){
+    return(
+        <button className="appButton" onClick={console.log("Hello World")}>
+        Click me
+        </button>
+    );
+
+}
+
+export default Button ;
